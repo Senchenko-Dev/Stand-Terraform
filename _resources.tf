cf289da6160d0822434d06360f4dc52d3a72ca73
sentsov.a.a <Sentsov.A.A@Sberbank.ru>
2022-10-03 11:25:46 +0300

    new secret / remove service projects

--- _resources.tf
+++ _resources.tf
@@ -87,7 +87,7 @@ locals {
 module "diOpenshiftServiceCore"  {
   source = "./modules/ansible_project_init"
 //  count = 0
-  for_each = local.diOpenshiftServiceCore_projects
+  for_each = {} #local.diOpenshiftServiceCore_projects
   managment_system_type = var.managment_system_type
   project_name = each.value.oseProjectName
   kubeconfig = local.oc_kubeconfig
