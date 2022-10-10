faf81492f87de9d30e363501896fd62a4019ba9c
sentsov.a.a <Sentsov.A.A@Sberbank.ru>
2022-10-10 13:33:21 +0300

    test iag fix secrets file

--- modules/spo_nginx_iag/variables.tf
+++ modules/spo_nginx_iag/variables.tf
@@ -2,41 +2,35 @@
 variable "vm_count" {
   default = 1
 }
-
 variable "vm_props" {}
 variable "vm_disk_data" {
   default = []
 }
-
-variable "spo_role_name" {
-  default = "nginx_iag"
-}
-
 variable "memory" {
   description = "RAM of Virtual Machine"
   type = number
   default = 1024
 }
-
 variable "cpu" {
   description = "CPU of Virtual Machine"
   type = number
   default = 1
 }
 
-//variable "nexus_cred" {}
-
 variable "inventory_group_name" {
   description = "inventory_group_name"
   type = string
 }
-
+variable "vault_file" {
+  description = "Имя файла с зашифрованными переменными, расположенного по пути ./ansible/"
+}
 variable "force_ansible_run" {
   default = false
   description = "Для принудительного запуска ансибл изменить значение"
 }
-
-
+variable "spo_role_name" {
+  default = "nginx_iag"
+}
 variable "nginx_iag_url" {
 //  default = ""
 }
