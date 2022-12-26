f247f8f26aa01c495034d6783e1fb40a450bcb74
Никита Сенченко <nnsenchenko@sberbank.ru>
2022-12-26 13:53:04 +0700

    test new plugin helm

--- providers.tf
+++ providers.tf
@@ -14,6 +14,10 @@ terraform {
   }
 
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 1.1.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.8.0"
