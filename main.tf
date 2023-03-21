e74e5e409f1364dbb5da20d51ba8591c6f25d63a
20084516 <smsilimyankin@sberbank.ru>
2023-03-21 09:51:15 +0300

    add images

--- main.tf
+++ main.tf
@@ -13,8 +13,17 @@ locals {
   ]
   # параметры для VCD_VM
   vm_props_default = {
-    template_name = "altlinux-sp8-5.4.145.c9f1.old"
-    catalog_name = "ALTLINUX"
+    #---------CentOs---------#
+    template_name = "SBT-SPO-RHEL84-latest"
+    catalog_name = "SBT_CREATOR_TEMPLATES"
+
+    #--------AltLinux--------#
+    #template_name = "altlinux-sp8-5.4.145.c9f1.old"
+    #catalog_name = "ALTLINUX"
+
+    #--------SberLinux-------#
+    #template_name = "SBT-SPO-SBEL86-latest"
+    #catalog_name = "SBT_CREATOR_TEMPLATES"
     network_type = "org"
     ip_allocation_mode = "POOL"
 
