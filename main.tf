28d2a016eb25169fe8669a483ddd6cb75609a81a
Никита Сенченко <nnsenchenko@sberbank.ru>
2023-04-04 11:59:19 +0700

    Install pg 1

--- main.tf
+++ main.tf
@@ -19,12 +19,12 @@ locals {
 #    catalog_name = "RHEL7"
 
     #-------------SberLinux-----------------#
-     template_name = "SBT-SPO-SBEL86-latest"
-     catalog_name = "SBT_CREATOR_TEMPLATES"
+#     template_name = "SBT-SPO-SBEL86-latest"
+#     catalog_name = "SBT_CREATOR_TEMPLATES"
 
     #--------------CentOs7------------------#
-#    template_name = "CentOS7_64-bit"
-#    catalog_name = "Linux Templates"
+    template_name = "CentOS7_64-bit"
+    catalog_name = "Linux Templates"
 
     network_type = "org"
     ip_allocation_mode = "POOL"
